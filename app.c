ff723433c712dd15217c2fe8bbdeff314992f47c
Ryunosuke TAKEDA <takkerue@gmail.com>
2015-05-09 16:20:02 +0900

    use cyclic handler

--- app.c
+++ app.c
@@ -21,14 +21,14 @@
 
 // 内部関数プロトタイプ宣言
 static void LcdClear(void);
+static int ClockGetTime();
+static void ClockReset();
 
 /**
  * @brief メインタスク エントリポイント
  * @detail EV3RT OSのMAIN_TASKエントリーポイント
  */
 void main_task(intptr_t unused) {
-	int32_t timeCount = 0;
-
 	int32_t fontHeight = 0;
 	int32_t fontWidth = 0;
 	ev3_sensor_config(EV3_PORT_1, TOUCH_SENSOR);
@@ -37,7 +37,6 @@ void main_task(intptr_t unused) {
 	ev3_font_get_size(EV3_FONT_MEDIUM, &fontWidth, &fontHeight);
 	LcdClear();
 	while(1) {
-		++timeCount;
 		if (ev3_button_is_pressed(LEFT_BUTTON)) {
 			ev3_led_set_color(LED_RED);
 		} else if (ev3_button_is_pressed(RIGHT_BUTTON)) {
@@ -51,14 +50,43 @@ void main_task(intptr_t unused) {
 	ev3_lcd_draw_string("TEXT", 0, (int32_t)(fontHeight * 1.5 * 1));
 	{
 		char text[100];
-		sprintf(text, "%d", timeCount);
+		sprintf(text, "%d", ClockGetTime());
 		ev3_lcd_draw_string(text, 0, (int32_t)(fontHeight * 1.5 * 2));
 	}
 	tslp_tsk(10);
 	}
 }
 
-void LcdClear(void) {
+static int clockCount = 0;
+
+/**
+ * @bried クロック計数周期ハンドラのエントリポイント
+ * @note 1ms周期でカウントアップ
+ * @param 周期ハンドラID
+ */
+void clock_cychdr(intptr_t idx) {
+	++clockCount;
+	return;
+}
+
+/**
+ * @brief タイマカウンタの取得
+ * @return タイマカウンタ[ms]
+ */
+static int ClockGetTime()
+{
+	return clockCount;
+}
+
+/**
+ * @brief タイマカウンタのリセット
+ */
+static void ClockReset()
+{
+	clockCount = 0;
+}
+
+static void LcdClear(void) {
 	ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE); // Clear menu area
 	return;
 }
