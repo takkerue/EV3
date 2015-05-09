b82a22cdfe1134f22141e5c47ec7bc73dee36709
Ryunosuke TAKEDA <takkerue@gmail.com>
2015-05-09 15:59:01 +0900

    LCD表示の追加

--- app.c
+++ app.c
@@ -19,13 +19,25 @@
 #define _debug(x)
 #endif
 
+// 内部関数プロトタイプ宣言
+static void LcdClear(void);
+
 /**
  * @brief メインタスク エントリポイント
  * @detail EV3RT OSのMAIN_TASKエントリーポイント
  */
 void main_task(intptr_t unused) {
+	int32_t timeCount = 0;
+
+	int32_t fontHeight = 0;
+	int32_t fontWidth = 0;
 	ev3_sensor_config(EV3_PORT_1, TOUCH_SENSOR);
+
+	ev3_lcd_set_font(EV3_FONT_MEDIUM);
+	ev3_font_get_size(EV3_FONT_MEDIUM, &fontWidth, &fontHeight);
+	LcdClear();
 	while(1) {
+		++timeCount;
 		if (ev3_button_is_pressed(LEFT_BUTTON)) {
 			ev3_led_set_color(LED_RED);
 		} else if (ev3_button_is_pressed(RIGHT_BUTTON)) {
@@ -33,8 +45,20 @@ void main_task(intptr_t unused) {
 		} else if (ev3_touch_sensor_is_pressed(EV3_PORT_1)) {
 			ev3_led_set_color(LED_ORANGE);
 		} else {
-		//	ev3_led_set_color(LED_OFF);
+			ev3_led_set_color(LED_OFF);
 		}
+	ev3_lcd_draw_string("TITLE", 0, (int32_t)(fontHeight * 1.5 * 0));
+	ev3_lcd_draw_string("TEXT", 0, (int32_t)(fontHeight * 1.5 * 1));
+	{
+		char text[100];
+		sprintf(text, "%d", timeCount);
+		ev3_lcd_draw_string(text, 0, (int32_t)(fontHeight * 1.5 * 2));
+	}
 	tslp_tsk(10);
 	}
+}
+
+void LcdClear(void) {
+	ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE); // Clear menu area
+	return;
 }
